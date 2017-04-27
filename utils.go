d7fc4532e60405146ed8da2c5275fafc9106facf
Lanre Adelowo <me@lanreadelowo.com>
2017-04-27 04:15:32 +0100

    Utility function to increment integers of all types

--- utils.go
+++ utils.go
@@ -35,3 +35,41 @@ func BytesToItem(data []byte) (*Item, error) {
 
 	return i, err
 }
+
+func Increment(val interface{}, steps int) (interface{}, error) {
+
+	var ret interface{}
+
+	switch val.(type) {
+
+	case int:
+		ret = val.(int) + steps
+
+	case int32:
+		ret = val.(int32) + int32(steps)
+
+	case int64:
+		ret = val.(int64) + int64(steps)
+
+	case uint:
+		ret = val.(uint) + uint(steps)
+
+	case uint8:
+		ret = val.(uint8) + uint8(steps)
+
+	case uint16:
+		ret = val.(uint16) + uint16(steps)
+
+	case uint32:
+		ret = val.(uint32) + uint32(steps)
+
+	case uint64:
+		ret = val.(uint64) + uint64(steps)
+
+	default:
+		return 0, ErrCacheDataCannotBeIncreased
+	}
+
+	return ret, nil
+
+}
