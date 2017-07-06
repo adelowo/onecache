bcfd809c7d048a15e57f027df23f9397bce6d627
Lanre Adelowo <me@lanreadelowo.com>
2017-07-06 12:14:50 +0100

    Reverted to a manually written mock

--- extend_test.go
+++ extend_test.go
@@ -3,13 +3,12 @@ package onecache
 import (
 	"reflect"
 	"testing"
-
-	"github.com/adelowo/onecache/mocks"
+	"time"
 )
 
 func TestExtensibility(t *testing.T) {
 
-	dummyStore := &mocks.Store{}
+	dummyStore := &mock{}
 
 	Extend("dummy", func() Store {
 		return dummyStore
@@ -24,4 +23,30 @@ func TestExtensibility(t *testing.T) {
 	if !reflect.DeepEqual(dummyStore, s) {
 		t.Fatalf("Stores differ...Expected %v \n Got %v", dummyStore, s)
 	}
+}
+
+type mock struct {
+}
+
+func (_m *mock) Delete(key string) error {
+	return nil
+}
+
+func (_m *mock) Flush() error {
+	return nil
+}
+
+// Get provides a mock function with given fields: key
+func (_m *mock) Get(key string) ([]byte, error) {
+	return nil, nil
+}
+
+// Has provides a mock function with given fields: key
+func (_m *mock) Has(key string) bool {
+	return true
+}
+
+// Set provides a mock function with given fields: key, data, expires
+func (_m *mock) Set(key string, data []byte, expires time.Duration) error {
+	return nil
 }
